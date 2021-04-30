6c2f0feaf8d0fc1d8551eb632cf514e208b73ebb
seunghaekim <shkim@ilark.co.kr>
2021-04-30 11:46:42 +0900

    init

---
+++ js/common.js
@@ -0,0 +1,26 @@
+ $(".tab > ul > li").click(function(){
+                $("li").removeClass('on');
+                $(this).addClass('on');
+            }); $(".tab > ul > li").click(function(){
+                $("li").removeClass('on');
+                $(this).addClass('on');
+            }); $(".tab > ul > li").click(function(){
+                $("li").removeClass('on');
+                $(this).addClass('on');
+            }); $(".tab > ul > li").click(function(){
+                $("li").removeClass('on');
+                $(this).addClass('on');
+            }); $(".tab > ul > li").click(function(){
+                $("li").removeClass('on');
+                $(this).addClass('on');
+            });
+
+	$(".sub").hover(function(){
+		$(this).children(".submenu").css('display','block');
+		$(".subbg").css('display','block');
+	});
+	$(".sub").mouseleave(function(){
+		$(this).children(".submenu").css('display','none');
+		$(".subbg").css('display','none');
+	});
+
