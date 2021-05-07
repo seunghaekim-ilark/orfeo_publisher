257e5b91709a125859a9605f9b32f543ec7665b9
seunghaekim <shkim@ilark.co.kr>
2021-05-07 16:51:45 +0900

    update some

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
+import dayjs from 'dayjs'
 import fs from 'fs'
 import { JSDOM } from 'jsdom'
+
+function info (name) {
+    const path = `${htmlDir}/${name}`
+    return fs.statSync(path)
+}
 
 const htmlDir = './html'
 const htmlFiles = fs.readdirSync(htmlDir).filter(item => {
     return item.match(/.+\.html$/)
+}).map(name => {
+    return {
+        name,
+        info: info(name)
+    }
 })
-
-const dom = new JSDOM(`
-    <body>
-        <ul class="list-index"></ul>
-    </body>
-`)
+const template = fs.readFileSync('./template.html')
+const dom = new JSDOM(template.toString())
 
 const document = dom.window.document
-const listIndex = document.querySelector('.list-index')
+const table = document.querySelector('.list-index')
+const tbody = table.querySelector('tbody')
+
+function td (content) {
+    const td = document.createElement('td')
+    if (content) {
+        td.innerHTML = content
+    }
+    return td
+}
 
 htmlFiles.forEach(item => {
-    const li = document.createElement('li')
-
+    const tr = document.createElement('tr')
+    
+    const filename = td()
     const anchor = document.createElement('a')
-    anchor.innerHTML = item
-    anchor.setAttribute('href', `./html/${item}`)
+    anchor.innerHTML = item.name
+    anchor.setAttribute('href', `./html/${item.name}`)
+    filename.appendChild(anchor)
 
-    li.appendChild(anchor)
-    listIndex.appendChild(li)
+    const format = 'YYYY/MM/DD hh:mm'
+
+    tr.appendChild(filename)
+    tr.appendChild(td( dayjs(item.info.ctime).format(format) ))
+    tr.appendChild(td( dayjs(item.info.mtime).format(format) ))
+    tr.appendChild(td( dayjs(item.info.birthtime).format(format) ))
+
+    tbody.appendChild(tr)
 })
 
 fs.writeFileSync(`./index.html`, dom.serialize())
